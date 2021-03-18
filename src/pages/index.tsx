d7a010cb05a98ca13f55f72e84929e36ad093a55
gabriel6181997 <gabriel6181997@gmail.com>
2021-03-18 17:14:36 +0800

    Login Page Layout Done

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -6,41 +6,62 @@ import { PrimaryButton } from "src/components/shared/PrimaryButton";
 const IndexPage = () => {
   return (
     <>
-      <DarkModeSwitch />
-      <Image src="/img/temporary_logo.jpg" width={200} height={200} />
-      <p>ログイン</p>
-      <div>
-        <Input
-          id="email"
-          placeholder="ユーザー名/メールアドレス"
-          variant="underlined"
-        />
-
-        <Input
-          id="password"
-          placeholder="パスワード"
-          variant="underlined"
-        />
+      <div className=" mt-3 text-right mr-10  md:mr-20">
+        <DarkModeSwitch />
       </div>
+      <div className="text-center mt-7">
+        <Image src="/img/temporary_logo.jpg" width={200} height={200} />
+        <p className="mt-5 text-2xl font-bold">ログイン</p>
 
-      <div>
-        <PrimaryButton
-          className=""
-          variant="solid"
-          linkProps={{ href: "/home/index" }}
-          >
-          ログイン
-        </PrimaryButton>
+        <div className="mt-10">
+          <div className="w-72 mx-auto">
+            <Input
+              id="email"
+              placeholder="ユーザー名/メールアドレス"
+              variant="underlined"
+            />
+          </div>
 
-        <PrimaryButton
-          className=""
-          variant="solid"
-          linkProps={{ href: "/home/index" }}
-          >
-          テストユーザー
-        </PrimaryButton>
+          <div className="w-72 mx-auto mt-4">
+            <Input
+              id="password"
+              placeholder="パスワード"
+              variant="underlined"
+            />
+          </div>
+        </div>
 
+        <div className="flex flex-col mt-7">
+          <div>
+            <PrimaryButton
+              className="px-20 py-2 my-1 text-xl"
+              variant="solid"
+              linkProps={{ href: "/" }}
+            >
+              ログイン
+            </PrimaryButton>
+          </div>
 
+          <div className="mt-3">
+            <PrimaryButton
+              className="px-12 py-2 my-1 text-xl"
+              variant="solid"
+              linkProps={{ href: "/home" }}
+            >
+              テストユーザー
+            </PrimaryButton>
+          </div>
+
+          <div className="mt-3">
+            <PrimaryButton
+              className="px-3 py-1"
+              variant="ghost"
+              linkProps={{ href: "/" }}
+            >
+              アカウントをお持ちでない方は<br />こちらへ
+            </PrimaryButton>
+          </div>
+        </div>
       </div>
     </>
   );
