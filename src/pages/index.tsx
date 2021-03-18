1a46531f78b3940e437e28ae9d0bfd2e3b4a0fea
gabriel6181997 <gabriel6181997@gmail.com>
2021-03-18 16:08:43 +0800

    Add items to index.tsx

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,48 @@
+import Image from "next/image";
+import { DarkModeSwitch } from "src/components/separate/DarkModeSwitch";
+import { Input } from "src/components/shared/Input";
+import { PrimaryButton } from "src/components/shared/PrimaryButton";
+
 const IndexPage = () => {
   return (
-    <h1>LoginPage</h1>
+    <>
+      <DarkModeSwitch />
+      <Image src="/img/temporary_logo.jpg" width={200} height={200} />
+      <p>ログイン</p>
+      <div>
+        <Input
+          id="email"
+          placeholder="ユーザー名/メールアドレス"
+          variant="underlined"
+        />
+
+        <Input
+          id="password"
+          placeholder="パスワード"
+          variant="underlined"
+        />
+      </div>
+
+      <div>
+        <PrimaryButton
+          className=""
+          variant="solid"
+          linkProps={{ href: "/home/index" }}
+          >
+          ログイン
+        </PrimaryButton>
+
+        <PrimaryButton
+          className=""
+          variant="solid"
+          linkProps={{ href: "/home/index" }}
+          >
+          テストユーザー
+        </PrimaryButton>
+
+
+      </div>
+    </>
   );
 };
 
