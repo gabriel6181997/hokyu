61b48e16792e61f48c874befccb1d1c23bdc1a03
gabriel6181997 <gabriel6181997@gmail.com>
2021-03-22 16:02:02 +0800

    Minor Change in Header.tsx

--- src/components/separate/Header.tsx
+++ src/components/separate/Header.tsx
@@ -1,11 +1,16 @@
+import type { VFC } from "react";
 import React from 'react';
-import { DarkModeSwitch } from "src/components/separate/DarkModeSwitch"
+import { DarkModeSwitch } from "src/components/separate/DarkModeSwitch";
 
-export const Header = () => {
+type Props = {
+  title: string;
+}
+
+export const Header: VFC<Props> = (props) => {
 
   return (
     <header className="flex items-center h-14 md:h-20 justify-between bg-blue-200">
-      <h1 className="pl-8 font-bold text-2xl md:text-3xl">ホーム</h1>
+      <h1 className="pl-8 font-bold text-2xl md:text-3xl">{props.title}ホーム</h1>
       <div className="mr-10">
         <DarkModeSwitch />
       </div>
