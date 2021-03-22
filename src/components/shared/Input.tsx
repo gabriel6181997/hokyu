335f3c02078808062a81dc368ad3a4d9f141e965
gabriel6181997 <gabriel6181997@gmail.com>
2021-03-22 13:10:09 +0800

    Fix Input dark mode background

--- src/components/shared/Input.tsx
+++ src/components/shared/Input.tsx
@@ -14,7 +14,7 @@ type Props = {
 export const Input:VFC<Props> = (props) => {
   const className = cc([
     props.className,
-    "block w-full pl-2 bg-transparent focus:outline-none",
+    "block w-full pl-2 bg-transparent dark:bg-gray-900 focus:outline-none",
     {
       "border-b-2 focus:border-blue-400": props.variant === "underlined",
       "border-2 focus:border-blue-400": props.variant === "box",
