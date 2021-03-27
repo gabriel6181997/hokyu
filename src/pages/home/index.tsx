90cd4ddd7d4aba3432f55df3f93c69665b7c33ae
gabriel6181997 <gabriel6181997@gmail.com>
2021-03-27 15:34:27 +0800

    ✨ Add condition to prevent users from accessing to home without login

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,7 +1,21 @@
+//Import Libraries
+import { useEffect } from 'react'
+import { useRouter } from "next/router";
+
+//Import Components
+import { auth } from "src/firebase";
 import { Layout } from "src/components/separate/layout";
 import { ToddlerItems } from "src/components/separate/ToddlersItems";
 
 const IndexPage = () => {
+  const router = useRouter();
+
+  useEffect(()=> {
+    if (!auth.currentUser){
+      router.push('/')
+    };
+  },[auth.currentUser])
+
   return (
     <Layout addbutton sideMenu buttonNavigation title="ホーム">
       <ToddlerItems />
