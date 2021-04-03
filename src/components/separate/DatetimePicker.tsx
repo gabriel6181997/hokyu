920311c5fca6f7f07469bfa5e57947a1184f9275
gabriel6181997 <gabriel6181997@gmail.com>
2021-04-03 12:12:17 +0800

    ✨ Add New component DatetimePicker

---
+++ src/components/separate/DatetimePicker.tsx
@@ -0,0 +1,62 @@
+import {
+  KeyboardDateTimePicker,
+  MuiPickersUtilsProvider,
+} from "@material-ui/pickers";
+import DateFnsUtils from "@date-io/date-fns";
+import { useState } from "react";
+
+export const DatetimePicker = () => {
+  const [selectedDate, handleDateChange] = useState(
+    new Date("2018-01-01T00:00:00.000Z")
+  );
+
+  // const [datetime, setDatetime] = useState(new Date());
+
+  // const inputDatetime: InputHTMLAttributes<HTMLInputElement>["onChange"] = (
+  //   e
+  // ) => {
+  //   setDatetime(Number(e.target.value));
+  // };
+
+    // const formatDatetime = (numberValue: number) => {
+  //   const stringValue = numberValue.toString();
+  //   if (stringValue.length < 12) {
+  //     return;
+  //   }
+  //   const year = stringValue.slice(0, 4);
+  //   const month = stringValue.slice(4, 6);
+  //   const date = stringValue.slice(6, 8);
+  //   const hour = stringValue.slice(8, 10);
+  //   const minute = stringValue.slice(10, 12);
+  //   const value = `${year}-${month}-${date} ${hour}:${minute}`;
+  //   const formatedDatetime = format(new Date(value), "yyyy年M月d日 HH:mm");
+  //   console.log(formatedDatetime);
+  //   return formatedDatetime;
+  // };
+
+
+  return (
+    <div>
+      {/* <Input
+            type="text"
+            id="datetime"
+            placeholder="例：11:15"
+            onChange={inputDatetime}
+            variant="underlined"
+            value={datetime}
+      /> */}
+
+      <MuiPickersUtilsProvider utils={DateFnsUtils}>
+        <KeyboardDateTimePicker
+          variant="inline"
+          ampm={false}
+          value={selectedDate}
+          onChange={handleDateChange}
+          onError={console.log}
+          disablePast
+          format="yyyy/MM/dd HH:mm"
+        />
+      </MuiPickersUtilsProvider>
+    </div>
+  );
+};
