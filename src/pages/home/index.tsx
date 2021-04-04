595e069d291bb9806d3a4b13dae95064392483e6
gabriel6181997 <gabriel6181997@gmail.com>
2021-04-04 17:34:39 +0800

    💄 Slightly change Responsive UI

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -45,7 +45,7 @@ const IndexPage = () => {
           >
             <Link href="/">
               <a className="flex items-center py-3">
-                <div className="w-32 h-32 pl-4 relative">
+                <div className="w-32 h-32 pl-4 relative mt-3">
                   <img
                     src={data.toddlerphoto}
                     alt={data.name}
@@ -55,12 +55,12 @@ const IndexPage = () => {
                     {data.gender === "male" ? (<div className="text-blue-400"><GiMale /></div>) : (<div className="text-rose-300"><GiFemale /></div>)}
                   </div>
                 </div>
-                <div className="w-3/6 pl-3">
+                <div className="w-3/6 ml-6">
                   <h2 className="text-xl md:text-2xl">{data.name}</h2>
                   <p className="text-lg pt-1 md:pt-3">{data.age}歳</p>
                 </div>
-                <div className="items-center w-1/6 text-center">
-                  <p>緊急度:</p>
+                <div className="items-center mr-3 text-center">
+                  <p className="ms:text-xs">緊急度:</p>
                   <p className="text-3xl md:text-5xl">{data.urgency}</p>
                 </div>
                 <p className="w-1/6 text-center text-2xl md:text-4xl">〉</p>
