76192ac3fc1cde04a0d467b6be73ff9d3d3a679c
gabriel6181997 <gabriel6181997@gmail.com>
2021-04-07 13:22:12 +0800

    🔥 Remove Unnecessary Import Codes

--- src/pages/home/mypage.tsx
+++ src/pages/home/mypage.tsx
@@ -1,24 +1,15 @@
 //Import Libraries
 import { useRouter } from "next/router";
-import React, { useState, useEffect, InputHTMLAttributes } from "react";
+import React, { useState, useEffect } from "react";
 import firebase from "firebase/app";
-import { useForm } from "react-hook-form";
 
 //Import Components
-import { auth, db, storage } from "src/firebase";
+import { auth, db } from "src/firebase";
 import { Layout } from "src/components/separate/layout";
-import { PrimaryButton } from "src/components/shared/PrimaryButton";
-import { testUser } from "src/config/testuser";
-import { Input } from "src/components/shared/Input";
 import { UserInfo } from "src/components/separate/UserInfo";
 
 const myPage = () => {
-  const [isEdit, setIsEdit] = useState(false);
   const [userInfo, setUserInfo] = useState<firebase.firestore.DocumentData>();
-  const [newName, setNewName] = useState("");
-  const [newUsername, setNewUsername] = useState("");
-  const [newProfileImageFile, setNewProfileImageFile] = useState<any>(null);
-  const [progress, setProgress] = useState(0);
   const user = auth.currentUser;
   const router = useRouter();
 
