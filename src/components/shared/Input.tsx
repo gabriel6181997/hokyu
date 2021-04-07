87ef62537765fd86ae393a3a990bd52cb71268e4
gabriel6181997 <gabriel6181997@gmail.com>
2021-04-07 13:14:22 +0800

    ⚡️ Change code for the sake of React Hook Form

--- src/components/shared/Input.tsx
+++ src/components/shared/Input.tsx
@@ -1,42 +1,48 @@
 import cc from "classcat";
-import type {InputHTMLAttributes, TextareaHTMLAttributes, VFC} from "react";
+import type { InputHTMLAttributes, TextareaHTMLAttributes, VFC } from "react";
+import { forwardRef } from "react";
+import type { UseFormRegisterReturn } from "react-hook-form";
 import TextAreaAutoSize from "react-textarea-autosize";
 
-type Props = {
-  className?:string;
+type Props = UseFormRegisterReturn & {
+  className?: string;
   error?: string;
-  id:string;
-  placeholder?:string;
-  onChange?: InputHTMLAttributes<HTMLInputElement>["onChange"];onChangeTextarea?:TextareaHTMLAttributes<HTMLTextAreaElement>["onChange"];
-  textarea?:boolean;
-  type?:string;
+  id: string;
+  placeholder?: string;
+  onChange?: InputHTMLAttributes<HTMLInputElement>["onChange"];
+  onChangeTextarea?: TextareaHTMLAttributes<HTMLTextAreaElement>["onChange"];
+  textarea?: boolean;
+  type?: string;
   variant: "underlined" | "box";
-  value?:string | number;
+  value?: string | number;
+  defaultValue?: string;
 };
 
-export const Input:VFC<Props> = (props) => {
+export const Input: VFC<Props> = forwardRef((props, ref) => {
   const className = cc([
     props.className,
     "block w-full pl-2 bg-transparent dark:bg-gray-900 focus:outline-none",
     {
       "border-b-2 focus:border-blue-400": props.variant === "underlined",
       "border-2 focus:border-blue-400": props.variant === "box",
-    }
+    },
   ]);
 
-  return(
+  return (
     <>
-      {props.textarea? (
+      {props.textarea ? (
         <TextAreaAutoSize
-         id={props.id}
-         name={props.id}
-         onChange={props.onChangeTextarea}
-         placeholder={props.placeholder}
-         className={className}
-         minRows={3}
-         value={props.value}
+          id={props.id}
+          name={props.id}
+          onChange={props.onChangeTextarea}
+          placeholder={props.placeholder}
+          className={className}
+          minRows={3}
+          value={props.value}
+          defaultValue={props.defaultValue}
+          ref={ref}
         />
-        ) : (
+      ) : (
         <input
           type={props.type}
           id={props.id}
@@ -45,9 +51,12 @@ export const Input:VFC<Props> = (props) => {
           className={className}
           onChange={props.onChange}
           value={props.value}
+          defaultValue={props.defaultValue}
+          ref={ref}
         />
-        )
-      }
+      )}
     </>
   );
-};
+});
+
+Input.displayName === "Input";
