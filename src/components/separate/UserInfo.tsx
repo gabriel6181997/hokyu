190a7ebe400f90f5b33d0de997d5865fee9837c1
gabriel6181997 <gabriel6181997@gmail.com>
2021-04-07 16:37:35 +0800

    🔥 Remove Unncessary Import Code

--- src/components/separate/UserInfo.tsx
+++ src/components/separate/UserInfo.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React, { useState } from "react";
 import { useForm } from "react-hook-form";
 import { FaCamera } from "react-icons/fa";
 import { PrimaryButton } from "../shared/PrimaryButton";
