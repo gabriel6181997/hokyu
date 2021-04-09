d44ebfd99af78ea73f43a18272ca4ad417ae48c5
gabriel6181997 <gabriel6181997@gmail.com>
2021-04-09 15:09:07 +0800

    ✨ Add RecoilRoot

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,14 +1,17 @@
-import 'src/styles/global.css';
-import '@fortawesome/fontawesome-free/css/all.css';
-import type { AppProps } from 'next/app';
+import { RecoilRoot } from "recoil";
+import "src/styles/global.css";
+import "@fortawesome/fontawesome-free/css/all.css";
+import type { AppProps } from "next/app";
 import { ThemeProvider } from "next-themes";
 
-function MyApp({ Component, pageProps }:AppProps) {
+function MyApp({ Component, pageProps }: AppProps) {
   return (
-  <ThemeProvider attribute="class">
-    <Component {...pageProps} />
-  </ThemeProvider>
-  )
+    <RecoilRoot>
+      <ThemeProvider attribute="class">
+        <Component {...pageProps} />
+      </ThemeProvider>
+    </RecoilRoot>
+  );
 }
 
-export default MyApp
+export default MyApp;
