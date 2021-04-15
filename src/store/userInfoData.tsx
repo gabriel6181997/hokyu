0675b3bad4b93e57ad9bee8f7996f4be0bba54ca
gabriel6181997 <gabriel6181997@gmail.com>
2021-04-15 18:53:26 +0800

    ♻️ Use useRecoilState to refactor code

--- src/store/userInfoData.tsx
+++ src/store/userInfoData.tsx
@@ -1,9 +1,46 @@
-import { selector } from "recoil";
+import { useEffect } from "react";
+import { atom, useRecoilState } from "recoil";
 import { auth, db } from "src/firebase";
 
-export const userInfoData = selector({
-  key: "userInfo",
-  get: async ({ get }) => {
+// export const userInfoData = selector({
+//   key: "userInfo",
+//   get: async ({ get }) => {
+//     auth.onAuthStateChanged(async (user) => {
+//       if (user) {
+//         const response = await db.collection("users").doc(user.uid).get();
+
+//         let info = {
+//           name: "unknown",
+//           username: "unknown",
+//           profileImageFile: "no picture",
+//         };
+
+//         if (response.exists) {
+//           info = response.data();
+//         }
+
+//         // console.log(info);
+//       } else {
+//         alert("no user!");
+//       }
+//     });
+//   },
+// });
+
+export const userInfoState = atom({
+  key: "userInfoState",
+  default: {
+    name: "unknown",
+    username: "unknown",
+    profileImageFile: "no picture",
+  },
+});
+
+export const GetUserInfoData = () => {
+  const [userInfoData, setUserInfoData] = useRecoilState(userInfoState);
+
+  useEffect(() => {
+    // async () => {
     auth.onAuthStateChanged(async (user) => {
       if (user) {
         const response = await db.collection("users").doc(user.uid).get();
@@ -17,11 +54,14 @@ export const userInfoData = selector({
         if (response.exists) {
           info = response.data();
         }
-
         // console.log(info);
+        setUserInfoData(info);
+        // console.log(userInfoData)
       } else {
         alert("no user!");
       }
     });
-  },
-});
+    // }
+  }, [setUserInfoData]);
+
+};
