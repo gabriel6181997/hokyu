079175fc1c3e3642182edecefcf5efa6990c53f3
gabriel6181997 <gabriel6181997@gmail.com>
2021-04-22 21:15:59 +0800

    ✨ Add TestInputItem Component

---
+++ src/components/test/TestInputItem.tsx
@@ -0,0 +1,9 @@
+export const TestInputItem =  () => {
+  // const inputList = useRecoilValue(inputListState)
+
+  return(
+    <div>
+       <input type="text" className="block w-full pl-2 bg-transparent dark:bg-gray-900 focus:outline-none border-b-2 focus:border-blue-400" />
+    </div>
+  )
+}
