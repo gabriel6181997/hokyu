b8c1dd28339dd8113083ad5402cc3ce0949d8b3a
gabriel6181997 <gabriel6181997@gmail.com>
2021-05-30 17:16:56 +0800

    ✨ Add an input tag and a submit button

--- src/pages/home/add3.tsx
+++ src/pages/home/add3.tsx
@@ -1,31 +1,51 @@
 import { Layout } from "src/components/separate/Layout";
-import Typography from '@material-ui/core/Typography';
-import TodoForm from 'src/components/test/TodoForm';
+import Typography from "@material-ui/core/Typography";
+import TodoForm from "src/components/test/TodoForm";
 import TodoList from "src/components/test/TodoList";
-import useTodoState from 'src/components/test/useTodoState';
+import useTodoState from "src/components/test/useTodoState";
+import {  BiPlusCircle } from "react-icons/bi";
+import { useRecoilState } from "recoil";
+import { inputListState } from "src/store/inputListState";
 
 
 const Add3 = () => {
-  const { todos, addTodo, deleteTodo } = useTodoState([]);
+  const [inputList, setInputList] = useRecoilState(inputListState);
+
+  const handleOnClickAdd = () => {
+    setInputList((prevInputList )=> [
+      // const newInputList = [...prevInputList, ]
+      // return newInputList;
+
+    ])
+  }
+
 
   return (
     <Layout sideMenu buttonNavigation title="add3">
       <div className="text-center">
-        <Typography component="h1" variant="h2">
-          Todos
-        </Typography>
 
-        <TodoForm
-          saveTodo={(todoText) => {
-            const trimmedText = todoText.trim();
+        <div className="flex justify-between ">
+          <p className="text-sm text-gray-700 font-medium dark:text-white">
+            体温
+          </p>
+          <button
+            className="text-gray-600 dark:text-white"
+            // onClick={handleOnClickAdd}
+          >
+            <BiPlusCircle />
+          </button>
+        </div>
 
-            if (trimmedText.length > 0) {
-              addTodo(trimmedText);
-            }
-          }}
-        />
+        {/* <TodoList todos={todos} deleteTodo={deleteTodo} /> */}
 
-        <TodoList todos={todos} deleteTodo={deleteTodo} />
+        <input className="block w-full pl-2 bg-transparent dark:bg-gray-900 focus:outline-none border-b-2 focus:border-blue-400 mt-4"/>
+
+        <button className="py-2  px-2 my-4 text-white rounded-3x1 shadow-md bg-blue-400 font-bold hover:bg-blue-300 duration-300 inline-flex items-center justify-center rounded-full" type="submit">体温を追加</button>
+
+
+
+
+
       </div>
     </Layout>
   );
