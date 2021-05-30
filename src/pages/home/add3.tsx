31fce491ed41180947d5865214c263899fd4053e
gabriel6181997 <gabriel6181997@gmail.com>
2021-05-30 18:49:54 +0800

    ✨ Use React Hook Form

--- src/pages/home/add3.tsx
+++ src/pages/home/add3.tsx
@@ -6,23 +6,31 @@ import useTodoState from "src/components/test/useTodoState";
 import {  BiPlusCircle } from "react-icons/bi";
 import { useRecoilState } from "recoil";
 import { inputListState } from "src/store/inputListState";
-
+import { useForm } from "react-hook-form";
 
 const Add3 = () => {
+  const {
+   handleSubmit,
+   register
+  } = useForm();
+
   const [inputList, setInputList] = useRecoilState(inputListState);
 
-  const handleOnClickAdd = () => {
-    setInputList((prevInputList )=> [
+  // const handleOnClickAdd = () => {
+    // setInputList((prevInputList )=> [
       // const newInputList = [...prevInputList, ]
       // return newInputList;
+  //   ])
+  // }
 
-    ])
-  }
+  const onSubmit = (data) => console.log(data);
 
 
   return (
     <Layout sideMenu buttonNavigation title="add3">
-      <div className="text-center">
+
+      <form className="text-center"
+      onSubmit={handleSubmit(onSubmit)}>
 
         <div className="flex justify-between ">
           <p className="text-sm text-gray-700 font-medium dark:text-white">
@@ -38,15 +46,14 @@ const Add3 = () => {
 
         {/* <TodoList todos={todos} deleteTodo={deleteTodo} /> */}
 
-        <input className="block w-full pl-2 bg-transparent dark:bg-gray-900 focus:outline-none border-b-2 focus:border-blue-400 mt-4"/>
+        <input className="block w-full pl-2 bg-transparent dark:bg-gray-900 focus:outline-none border-b-2 focus:border-blue-400 mt-4"
+        {...register("temperature")}
+        />
 
         <button className="py-2  px-2 my-4 text-white rounded-3x1 shadow-md bg-blue-400 font-bold hover:bg-blue-300 duration-300 inline-flex items-center justify-center rounded-full" type="submit">体温を追加</button>
 
 
-
-
-
-      </div>
+      </form>
     </Layout>
   );
 };
