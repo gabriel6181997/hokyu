c9a3da66fa535f6a4beb9cc72765e5ee76a45337
gabriel6181997 <gabriel6181997@gmail.com>
2021-05-31 14:23:59 +0800

    ✨ Add ToddlerPage

---
+++ src/pages/home/toddlers/[toddlerid].tsx
@@ -0,0 +1,15 @@
+import { Layout } from "src/components/separate/Layout";
+
+const ToddlerPage = () => {
+  return (
+   <Layout sideMenu buttonNavigation title="幼児詳細">
+     <div className="container space-y-6 pb-6">
+
+     </div>
+
+
+  </Layout>
+  )
+};
+
+export default ToddlerPage;
