caf84e036a005abbda3c355c71028e8fc715c055
gabriel6181997 <gabriel6181997@gmail.com>
2021-06-02 18:05:06 +0800

    ✨ Use React Hook Form

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,11 @@
 //Import Libraries
 import Image from "next/image";
 import { useRouter } from "next/router";
-import type { InputHTMLAttributes} from "react";
-import  { useState } from "react";
+import type { InputHTMLAttributes } from "react";
+import { useState } from "react";
+import { useForm } from "react-hook-form";
+import { ErrorMessage } from "@hookform/error-message";
+
 //Import Components
 import { DarkModeSwitch } from "src/components/separate/DarkModeSwitch";
 import { Input } from "src/components/shared/Input";
@@ -10,27 +13,31 @@ import { PrimaryButton } from "src/components/shared/PrimaryButton";
 import { testUser } from "src/config/testuser";
 import { auth } from "src/firebase";
 
+type Login = {
+  email: string;
+  password: string;
+}
 
 const IndexPage = () => {
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
   const router = useRouter();
 
-  const handleLogin = async (e: React.SyntheticEvent) => {
-    e.preventDefault();
+  const handleLogin = async (e: Login) => {
     await auth
-      .signInWithEmailAndPassword(email, password)
-      .then( () => {
+      .signInWithEmailAndPassword(e.email, e.password)
+      .then(() => {
         router.push("/home");
-        setEmail("");
-        setPassword("");
       })
       .catch(() => {
-        alert("入力された情報と登録された情報が一致しません");
+        alert("入力された情報と登録された情報は一致しません");
       });
   };
 
-  const handleTestLogin = async (e:React.SyntheticEvent) => {
+  const handleTestLogin = async (e: React.SyntheticEvent) => {
     e.preventDefault();
     await auth
       .signInWithEmailAndPassword(testUser.email, testUser.password)
@@ -40,14 +47,6 @@ const IndexPage = () => {
       .catch(() => {
         alert("テストログインに失敗しました");
       });
-  };
-
-  const handleInputEmail: InputHTMLAttributes<HTMLInputElement>["onChange"] = (e)  => {
-    setEmail(e.target.value);
-  };
-
-  const handleInputPassword: InputHTMLAttributes<HTMLInputElement>["onChange"] = (e) => {
-    setPassword(e.target.value);
   };
 
   return (
@@ -62,40 +61,49 @@ const IndexPage = () => {
           <h1 className="font-custom text-3xl pl-2">Hokyu</h1>
         </div>
 
-        <div className="mt-8">
+        <form className="mt-8" onSubmit={handleSubmit(handleLogin)}>
           <div className="w-72 mx-auto">
             <Input
               type="email"
-              id="email"
               placeholder="メールアドレス"
               variant="underlined"
-              onChange={handleInputEmail}
+              {...register("email", {
+                required: "メールアドレスを入力してください！",
+              })}
             />
           </div>
 
-          <div className="w-72 mx-auto mt-5">
+          <div className="text-rose-600 font-bold">
+            <ErrorMessage errors={errors} name="email" />
+          </div>
+
+          <div className="w-72 mx-auto mt-3">
             <Input
               type="password"
-              id="password"
               placeholder="パスワード"
               variant="underlined"
-              onChange={handleInputPassword}
+              {...register("password", {
+                required: "パスワードを入力してください！",
+              })}
             />
           </div>
-        </div>
 
-        <div className="flex flex-col mt-7">
-          <div>
+          <div className="text-rose-600 font-bold">
+            <ErrorMessage errors={errors} name="password" />
+          </div>
+
+          <div className="mt-5">
             <PrimaryButton
               button
               className="px-20 py-2 my-1 text-xl"
               variant="solid"
-              onClick={handleLogin}
             >
               ログイン
             </PrimaryButton>
           </div>
+        </form>
 
+        <div className="mt-3">
           <div className="mt-3">
             <PrimaryButton
               button
