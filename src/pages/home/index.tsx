94adcaadb2524661e91e39c0908fd071f964592c
gabriel6181997 <gabriel6181997@gmail.com>
2021-06-04 16:22:11 +0800

    ✨ Use animate-pulse

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -67,37 +67,39 @@ const IndexPage = () => {
         {toddlerInfos.map(({ id, data }) => {
           return (
             <>
-              <li
-                key={id}
-                className="border-b dark:border-gray-400 md:hover:bg-blue-50 duration-300 md:dark:hover:text-blue-400 md:dark:hover:bg-gray-50 md:dark:hover:bg-opacity-20"
-              >
-                <Link href="/toddlers/id">
-                  <ToddlerItem
-                    age={data.age}
-                    toddlerphoto={data.toddlerphoto}
-                    name={data.name}
-                    urgency={data.urgency}
-                    gender={data.gender}
-                  />
-                </Link>
-              </li>
+              {toddlerInfos ? (
+                <li
+                  key={id}
+                  className="border-b dark:border-gray-400 md:hover:bg-blue-50 duration-300 md:dark:hover:text-blue-400 md:dark:hover:bg-gray-50 md:dark:hover:bg-opacity-20"
+                >
+                  <Link href="/toddlers/id">
+                    <ToddlerItem
+                      age={data.age}
+                      toddlerphoto={data.toddlerphoto}
+                      name={data.name}
+                      urgency={data.urgency}
+                      gender={data.gender}
+                    />
+                  </Link>
+                </li>
+              ) : (
+                <div className="shadow rounded-md p-4  w-full mx-auto">
+                  <div className="animate-pulse flex">
+                    <div className="rounded-full bg-gray-200 h-28 w-28 mr-7"></div>
+                    <div className="ms:w-1/3 w-1/2 space-y-5 py-7">
+                      <div className="w-24 h-6 bg-gray-200 rounded"></div>
+                      <div className="space-y-2 w-11 h-4 bg-gray-200 rounded "></div>
+                    </div>
 
-              {/* <div className="shadow rounded-md p-4  w-full mx-auto">
-                <div className="animate-pulse flex">
-                  <div className="rounded-full bg-gray-200 h-28 w-28 mr-7"></div>
-                  <div className="ms:w-1/3 w-1/2 space-y-5 py-7">
-                    <div className="w-24 h-6 bg-gray-200 rounded"></div>
-                    <div className="space-y-2 w-11 h-4 bg-gray-200 rounded "></div>
+                    <div className=" space-y-3 py-7">
+                      <div className="ml-2 h-4 bg-gray-200 rounded w-12"></div>
+                      <div className="ml-3 h-11 space-y-2 bg-gray-200 rounded w-10"></div>
+                    </div>
+
+                    <div className="ml-auto bg-gray-200 w-8 h-14 mt-9"></div>
                   </div>
-
-                  <div className=" space-y-3 py-7">
-                    <div className="ml-2 h-4 bg-gray-200 rounded w-12"></div>
-                    <div className="ml-3 h-11 space-y-2 bg-gray-200 rounded w-10"></div>
-                  </div>
-
-                  <div className="ml-auto bg-gray-200 w-8 h-14 mt-9"></div>
                 </div>
-              </div> */}
+              )}
             </>
           );
         })}
