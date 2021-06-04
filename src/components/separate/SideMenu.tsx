e94562c6676fc6ce97275b31707b6016bd59ee38
gabriel6181997 <gabriel6181997@gmail.com>
2021-06-04 16:35:58 +0800

    💄 Add animate-pulse UI

--- src/components/separate/SideMenu.tsx
+++ src/components/separate/SideMenu.tsx
@@ -9,18 +9,18 @@ import { userInfoState } from "src/store/userInfoState";
 import { GLOBAL_MENUS } from "src/utils/constants/menu";
 
 export const SideMenu = () => {
-
-  const userInfoData = useRecoilValue(userInfoState)
+  const userInfoData = useRecoilValue(userInfoState);
 
   return (
-      <aside className="flex flex-col ml-auto w-48 sticky h-screen top-0">
-        <div className="ml-2 py-7 text-center flex items-center">
-          <Image src="/img/logo.png" alt="logo" width={50} height={50} />
-          <h1 className="font-custom text-3xl pl-2">Hokyu</h1>
-        </div>
-        <nav>
-          <ul>
-            {GLOBAL_MENUS.map(({ icon, label, href }) => {return (
+    <aside className="flex flex-col ml-auto w-48 sticky h-screen top-0">
+      <div className="ml-2 py-7 text-center flex items-center">
+        <Image src="/img/logo.png" alt="logo" width={50} height={50} />
+        <h1 className="font-custom text-3xl pl-2">Hokyu</h1>
+      </div>
+      <nav>
+        <ul>
+          {GLOBAL_MENUS.map(({ icon, label, href }) => {
+            return (
               <li key={label}>
                 <PrimaryButton
                   className="px-3 py-3 my-1 text-xl"
@@ -31,21 +31,43 @@ export const SideMenu = () => {
                   <p className="pl-4 font-bold">{label}</p>
                 </PrimaryButton>
               </li>
-            )})}
-          </ul>
-        </nav>
+            );
+          })}
+        </ul>
+      </nav>
 
-        <div className="px-4 pt-6">
-          <PrimaryButton
-            className="px-8 py-3"
-            variant="solid"
-            linkProps={{ href: "/home/add" }}
-          >
-            <BsPencilSquare />
-            <p className="pl-3">新規幼児</p>
-          </PrimaryButton>
-        </div>
+      <div className="px-4 pt-6">
+        <PrimaryButton
+          className="px-8 py-3"
+          variant="solid"
+          linkProps={{ href: "/home/add" }}
+        >
+          <BsPencilSquare />
+          <p className="pl-3">新規幼児</p>
+        </PrimaryButton>
+      </div>
 
+      <div className="px-4 pt-6">
+        <PrimaryButton
+          className="px-8 py-3"
+          variant="solid"
+          linkProps={{ href: "/home/add2" }}
+        >
+          <p className="pl-3">add2</p>
+        </PrimaryButton>
+      </div>
+
+      <div className="px-4 pt-6">
+        <PrimaryButton
+          className="px-8 py-3"
+          variant="solid"
+          linkProps={{ href: "/home/add3" }}
+        >
+          <p className="pl-3">add3</p>
+        </PrimaryButton>
+      </div>
+
+      {userInfoData ? (
         <div className="mt-auto mb-5 mx-auto flex items-center">
           <img
             src={userInfoData.profileImageFile}
@@ -59,6 +81,15 @@ export const SideMenu = () => {
             </p>
           </div>
         </div>
-      </aside>
+       ) : (
+        <div className="animate-pulse mt-auto mb-5 mx-auto flex items-center">
+          <div className="rounded-full bg-gray-200 h-11 w-11"></div>
+          <div className="pl-2">
+            <div className="bg-gray-200 w-20 h-3"></div>
+            <div className="bg-gray-200 w-20 h-3  mt-2"></div>
+          </div>
+        </div>
+       )}
+    </aside>
   );
 };
