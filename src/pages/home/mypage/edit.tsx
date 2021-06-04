a0d3d7c65a0a8df1dec1ecc9600c5a55ae68da67
gabriel6181997 <gabriel6181997@gmail.com>
2021-06-04 13:18:26 +0800

    💄 Update loading UI

--- src/pages/home/mypage/edit.tsx
+++ src/pages/home/mypage/edit.tsx
@@ -4,14 +4,19 @@ import { UserInfoEdit } from "src/components/separate/UserInfoEdit";
 import { userInfoState } from "src/store/userInfoState";
 
 const UserInfoEditPage = () => {
-  const userInfoData = useRecoilValue(userInfoState)
+  const userInfoData = useRecoilValue(userInfoState);
 
   return (
     <Layout addbutton sideMenu buttonNavigation title="マイページ">
       {userInfoData ? (
         <UserInfoEdit preloadedValues={userInfoData} />
       ) : (
-        <div>loading...</div>
+        <div className="text-center mx-auto pt-10">
+          <div className="mx-auto w-52 h-52 rounded-full bg-gray-200"></div>
+          <div className="w-72 mx-auto my-7 h-8 bg-gray-200"></div>
+          <div className="w-72 mx-auto mt-2 h-8 bg-gray-200"></div>
+          <div className="mt-6 px-20 h-11 bg-gray-200 w-52 mx-auto rounded-full"></div>
+        </div>
       )}
     </Layout>
   );
