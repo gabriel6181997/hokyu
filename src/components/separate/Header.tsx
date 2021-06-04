2418e3b96953402e279da527d9bab2513696f476
gabriel6181997 <gabriel6181997@gmail.com>
2021-06-04 17:29:51 +0800

    💄 Change header to sticky header

--- src/components/separate/Header.tsx
+++ src/components/separate/Header.tsx
@@ -1,5 +1,4 @@
 import type { VFC } from "react";
-import React from 'react';
 import { DarkModeSwitch } from "src/components/separate/DarkModeSwitch";
 
 type Props = {
@@ -9,7 +8,7 @@ type Props = {
 export const Header: VFC<Props> = (props) => {
 
   return (
-    <header className="flex items-center h-14 md:h-20 justify-between bg-blue-200">
+    <header className="flex items-center h-14 md:h-20 justify-between bg-blue-200 sticky top-0 z-100">
       <h1 className="pl-8 font-bold dark:text-gray-700 text-2xl md:text-3xl">{props.title}</h1>
       <div className="mr-10">
         <DarkModeSwitch />
