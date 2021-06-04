45b54510057885fd2fb1ea4ed0e6d334f33909d4
gabriel6181997 <gabriel6181997@gmail.com>
2021-06-04 16:15:22 +0800

    ✨ Add animate-pulse

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,15 +1,38 @@
 //Import Libraries
-import { useEffect, useState } from "react";
-import { useRouter } from "next/router";
+import type firebase from "firebase/app";
 import Link from "next/link";
-import firebase from "firebase/app";
-
+import { useRouter } from "next/router";
+import { useEffect, useState } from "react";
+import { Layout } from "src/components/separate/Layout";
+import { ToddlerItem } from "src/components/separate/ToddlersItem";
 //Import Components
 import { auth, db } from "src/firebase";
-import { Layout } from "src/components/separate/Layout";
-import {ToddlerItem} from "src/components/separate/ToddlersItem"
 
+type Doc = {
+  id: string;
+  data: {
+    age: number;
+    appetite: string;
+    breath: string;
+    cough: string;
+    diarrhea: string;
+    exercise: string;
+    face: string;
+    gender: string;
     mood: string;
+    name: string;
+    others: string | null;
+    skin: string;
+    sleep: string;
+    // temperature: {
+    //   temperature1: {
+
+    //   }
+    // };
+    toddlerphoto: string;
+    urgency: number;
+  };
+};
 
 const IndexPage = () => {
   const router = useRouter();
@@ -22,42 +45,65 @@ const IndexPage = () => {
     if (!auth.currentUser) {
       router.push("/");
     }
-  }, [auth.currentUser]);
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
 
   useEffect(() => {
     db.collection("toddlers")
       .orderBy("urgency", "desc")
       .get()
-      .then((toddler) =>
-        setToddlerInfos(
-          toddler.docs.map((doc) => ({ id: doc.id, data: doc.data() }))
-        )
-      );
+      .then((toddler) => {
+        return setToddlerInfos(
+          toddler.docs.map((doc) => {
+            return { id: doc.id, data: doc.data() };
+          })
+        );
+      });
   }, []);
 
   return (
     <Layout addbutton sideMenu buttonNavigation title="ホーム">
       <ul>
-        {toddlerInfos.map(({ id, data }) => (
-          <li
-            key={id}
-            className="border-b dark:border-gray-400 md:hover:bg-blue-50 duration-300 md:dark:hover:text-blue-400 md:dark:hover:bg-gray-50 md:dark:hover:bg-opacity-20"
-          >
-            <Link href="/" passHref>
-               <ToddlerItem
-                age={data.age}
-                toddlerphoto={data.toddlerphoto}
-                name={data.name}
-                urgency={data.urgency}
-                gender={data.gender}
-              />
-            </Link>
-          </li>
-        ))}
+        {toddlerInfos.map(({ id, data }) => {
+          return (
+            <>
+              <li
+                key={id}
+                className="border-b dark:border-gray-400 md:hover:bg-blue-50 duration-300 md:dark:hover:text-blue-400 md:dark:hover:bg-gray-50 md:dark:hover:bg-opacity-20"
+              >
+                <Link href="/toddlers/id">
+                  <ToddlerItem
+                    age={data.age}
+                    toddlerphoto={data.toddlerphoto}
+                    name={data.name}
+                    urgency={data.urgency}
+                    gender={data.gender}
+                  />
+                </Link>
+              </li>
+
+              <div className="shadow rounded-md p-4  w-full mx-auto">
+                <div className="animate-pulse flex">
+                  <div className="rounded-full bg-gray-200 h-28 w-28 mr-7"></div>
+                  <div className="ms:w-1/3 w-1/2 space-y-5 py-7">
+                    <div className="w-24 h-6 bg-gray-200 rounded"></div>
+                    <div className="space-y-2 w-11 h-4 bg-gray-200 rounded "></div>
+                  </div>
+
+                  <div className=" space-y-3 py-7">
+                    <div className="ml-2 h-4 bg-gray-200 rounded w-12"></div>
+                    <div className="ml-3 h-11 space-y-2 bg-gray-200 rounded w-10"></div>
+                  </div>
+
+                  <div className="ml-auto bg-gray-200 w-8 h-14 mt-9"></div>
+                </div>
+              </div>
+            </>
+          );
+        })}
       </ul>
     </Layout>
   );
 };
 
 export default IndexPage;
-
