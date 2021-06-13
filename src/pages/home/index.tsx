f0dedcfc25e3d5ce137036f3d6ae863febf6273f
gabriel6181997 <gabriel6181997@gmail.com>
2021-06-13 17:14:14 +0800

    🐛  Fix unexpected token error

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -10,10 +10,9 @@ import { auth, db } from "src/firebase";
 
 const IndexPage = () => {
   const router = useRouter();
-  const [
-    toddlerInfos,
-    setToddlerInfos,
-  ] = useState<firebase.firestore.DocumentData[]>([]);
+  const [toddlerInfos, setToddlerInfos] = useState<
+    firebase.firestore.DocumentData[]
+  >([]);
 
   useEffect(() => {
     if (!auth.currentUser) {
@@ -22,31 +21,34 @@ const IndexPage = () => {
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
-  useEffect(()=> {
-    const unSub = db.collection("toddlers").orderBy("urgency", "desc").onSnapshot((snapshot)=> {
-      setToddlerInfos(
-        // eslint-disable-next-line arrow-body-style
-        snapshot.docs.map((doc)=> ({
-          id: doc.id,
-          data:doc.data()
-        }))
-      )
+  useEffect(() => {
+    const unSub = db
+      .collection("toddlers")
+      .orderBy("urgency", "desc")
+      .onSnapshot((snapshot) => {
+        setToddlerInfos(
+          // eslint-disable-next-line arrow-body-style
+          snapshot.docs.map((doc) => ({
+            id: doc.id,
+            data: doc.data(),
+          }))
+        );
       });
 
     // eslint-disable-next-line arrow-body-style
     return () => unSub();
-  },[])
-
+  }, []);
 
   return (
     <Layout addbutton sideMenu buttonNavigation title="ホーム">
       <ul>
-          {toddlerInfos.map(({ id, data }) => {
-            return (
-              <li
-                key={id}
-                className="border-b dark:border-gray-400 md:hover:bg-blue-50 duration-300 md:dark:hover:text-blue-400 md:dark:hover:bg-gray-50 md:dark:hover:bg-opacity-20"
-              >
+        { toddlerInfos.length > 0 ?
+        toddlerInfos.map(({ id, data }) => {
+          return (
+            <li
+              key={id}
+              className="border-b dark:border-gray-400 md:hover:bg-blue-50 duration-300 md:dark:hover:text-blue-400 md:dark:hover:bg-gray-50 md:dark:hover:bg-opacity-20"
+            >
               <Link
                 href={{
                   pathname: "/home/toddlers/[slug]",
@@ -62,30 +64,31 @@ const IndexPage = () => {
                     gender={data.gender}
                   />
                 </a>
-                </Link>
-              </li>
-            );
-          })}
+              </Link>
+            </li>
+          );
+        })
+         :
+        <div className="shadow rounded-md p-4  w-full mx-auto">
+          <div className="animate-pulse flex">
+            <div className="rounded-full bg-gray-200 h-28 w-28 mr-7"></div>
+            <div className="ms:w-1/3 w-1/2 space-y-5 py-7">
+              <div className="w-24 h-6 bg-gray-200 rounded"></div>
+              <div className="space-y-2 w-11 h-4 bg-gray-200 rounded "></div>
+            </div>
+
+            <div className=" space-y-3 py-7">
+              <div className="ml-2 h-4 bg-gray-200 rounded w-12"></div>
+              <div className="ml-3 h-11 space-y-2 bg-gray-200 rounded w-10"></div>
+            </div>
+
+            <div className="ml-auto bg-gray-200 w-8 h-14 mt-9"></div>
+          </div>
+        </div>
+         }
       </ul>
     </Layout>
   );
 };
 
 export default IndexPage;
-
-{/* // <div className="shadow rounded-md p-4  w-full mx-auto">
-  //   <div className="animate-pulse flex">
-  //     <div className="rounded-full bg-gray-200 h-28 w-28 mr-7"></div>
-  //     <div className="ms:w-1/3 w-1/2 space-y-5 py-7">
-  //       <div className="w-24 h-6 bg-gray-200 rounded"></div>
-  //       <div className="space-y-2 w-11 h-4 bg-gray-200 rounded "></div>
-  //     </div>
-
-  //     <div className=" space-y-3 py-7">
-  //       <div className="ml-2 h-4 bg-gray-200 rounded w-12"></div>
-  //       <div className="ml-3 h-11 space-y-2 bg-gray-200 rounded w-10"></div>
-  //     </div>
-
-  //     <div className="ml-auto bg-gray-200 w-8 h-14 mt-9"></div>
-  //   </div>
-  // </div> */}
